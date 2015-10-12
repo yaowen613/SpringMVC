4ea1ebb77f47e556f70e480ff07d8ceea28daeef
茗香墨语 <yaowen424@126.com>
2015-10-12 10:13:47 +0800

    second commit  for SpringMVC03 code；

---
+++ springmvc03/src/com/yaowen/controller/HellController.java
@@ -0,0 +1,21 @@
+package com.yaowen.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HellController {
+
+	@RequestMapping(value="/hello.do")
+	public String hello(String userName,Model model){
+		
+		System.out.println(userName);
+		
+		model.addAttribute("helloword", "Hello:"+userName);
+		
+		return "index";
+	}
+	
+}
